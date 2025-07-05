ca5eb8d310c7878b5afeba5e1e48a28c21cf03b3
Boris Shminke <boris@shminke.com>
2025-07-05 18:35:24 +0200

    use actions

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export EMACS_TEST_JUNIT_REPORT=1
-$EMACS --batch -L . -L tests -l test-flymake-pyrefly \
+emacs --batch -L . -L tests -l test-flymake-pyrefly \
        -f ert-run-tests-batch-and-exit
